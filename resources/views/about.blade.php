48682475337aede7e7379b146c8ba17ff06b1183
Pagaduan, Tony <TonyPagaduan@quickenloans.com>
2019-09-15 16:24:54 -0400

    Added the templated blade layout and pages

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-4">
+            <h2>Tony</h2>
+            <p><img class="profile--image" src="assets/img/about1.jpg" alt="Tony"></p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Toni</h2>
+            <p><img class="profile--image" src="assets/img/about2.jpg" alt="Toni"></p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h2>Tone</h2>
+            <p><img class="profile--image" src="assets/img/about3.jpg" alt="Tone"></p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div>
+        </div>
+
+        <hr>
+@endsection
